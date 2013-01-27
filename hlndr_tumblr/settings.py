dc49aa26b2c974fc85ea848ba33ea8f7fb794885
animekraxe <yukibhuynh@gmail.com>
2013-01-27 14:03:52 -0800

    New test

--- hlndr_tumblr/settings.py
+++ hlndr_tumblr/settings.py
@@ -4,6 +4,8 @@ import os
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+AUTH_PROFILE_MODULE = 'users.UserProfile'
+
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
@@ -15,7 +17,7 @@ project_dir = os.path.dirname( os.path.abspath('__file__') )
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'hlndr_tumblr.db',                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(project_dir,'db/hlndr_tumblr.db'),                      # Or path to database file if using sqlite3.
 		'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -124,8 +126,8 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-#	'users',
-#	'dashboard',
+	'users',
+	'dashboard',
 )
 
 # A sample logging configuration. The only tangible logging
